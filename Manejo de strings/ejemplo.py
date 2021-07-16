13f7024ca91007c3b4133efdf7cda1c62bd9c728
xeneas <plague@outlook.es>
2021-07-15 20:41:59 -0400

    empaquetamiento

---
+++ Manejo de strings/ejemplo.py
@@ -0,0 +1,5 @@
+from main import MiClase
+
+# help(MiClase)
+print(MiClase.__doc__)
+print(MiClase.__init__.__doc__)
---
+++ Manejo de strings/main.py
@@ -0,0 +1,15 @@
+# docstrings.- Documentar en Python
+# documentar una clase
+
+class MiClase:
+    ''' Ejemplo de documentacion una clase'''
+    def __init__(self):
+        '''
+        metodo de inicion de nuestra clase
+        '''
+    def mi_metodo(self, param1, param2):
+        '''
+        Documentacion de  nuestro metodo
+        :param param1:
+        :param param2
+        '''
